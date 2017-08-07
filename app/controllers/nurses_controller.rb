bfebe52530274fa4f5768e9bf4bed657dc37ef90
Cristina Iannone <iannonecristina@gmail.com>
2017-08-07 16:53:52 +0100

    Add update nurse controller action

--- app/controllers/nurses_controller.rb
+++ app/controllers/nurses_controller.rb
@@ -7,9 +7,20 @@ class NursesController < ApplicationController
     end
   end
 
+  def update
+    @nurse = Nurse.find(params[:id])
+    if !@nurse.update_attributes(update_nurse_params)
+      render status: 400, json: {errors: @nurse.errors.full_messages}
+    end
+  end
+
   private
 
   def nurse_params
     params.require(:nurse).permit(:email, :first_name, :last_name, :phone_number, :verified, :sign_in_count, :role_id)
+  end
+
+  def update_nurse_params
+    params.require(:nurse).permit(:email, :first_name, :last_name, :phone_number, :role_id)
   end
 end
--- spec/controllers/nurses_controller_spec.rb
+++ spec/controllers/nurses_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe NursesController do
       expect(Nurse.count).to eq 1
     end
 
-    it "creates a nurse with a specific name" do
+    it "creates a nurse object with a specific name" do
       role = Role.create(name: "Test role")
       post :create, params:{ nurse: {email: "test@example.com", first_name: "John", last_name: "Doe", phone_number: "074-0000-0000", verified: false, sign_in_count: 0, role_id: role.id}}
       expect(Nurse.last.first_name).to eq "John"
@@ -20,7 +20,7 @@ RSpec.describe NursesController do
       expect(response.status).to eq 204
     end
 
-    context "when there are empty fields" do
+    context "when empty fields are submitted" do
       it "returns the related json error" do
         role = Role.create(name: "Test role")
         post :create, params:{ nurse: {email: "test@example.com", first_name: "John", last_name: nil, phone_number: "074-0000-0000", verified: false, sign_in_count: 0, role_id: role.id}}
@@ -33,10 +33,59 @@ RSpec.describe NursesController do
         expect(response.status).to eq 400
       end
 
-      it "does not create a nurse" do
+      it "does not create a nurse object" do
         role = Role.create(name: "Test role")
         post :create, params:{ nurse: {email: "test@example.com", first_name: "John", last_name: nil, phone_number: "074-0000-0000", verified: false, sign_in_count: 0, role_id: role.id}}
         expect(Nurse.count).to eq 0
+      end
+    end
+
+    describe "#update" do
+      it "updates a nurse object" do
+        role = Role.create(name: "Test role")
+        nurse = Nurse.create(email: "test@example.com", first_name: "John", last_name: "Doe", phone_number: "074-0000-0000", verified: false, sign_in_count: 0, role_id: role.id)
+        patch :update, params:{ id: nurse.id, nurse: {email:"test2@example.com"}}
+        nurse.reload
+        expect(nurse.email).to eq "test2@example.com"
+      end
+
+      it "does not update the 'sign in count' attribute" do
+        role = Role.create(name: "Test role")
+        nurse = Nurse.create(email: "test@example.com", first_name: "John", last_name: "Doe", phone_number: "074-0000-0000", verified: false, sign_in_count: 0, role_id: role.id)
+        patch :update, params:{ id: nurse.id, nurse: {sign_in_count: 2 }}
+        nurse.reload
+        expect(nurse.sign_in_count).to eq 0
+      end
+
+      it "does not update the 'verified' attribute" do
+        role = Role.create(name: "Test role")
+        nurse = Nurse.create(email: "test@example.com", first_name: "John", last_name: "Doe", phone_number: "074-0000-0000", verified: false, sign_in_count: 0, role_id: role.id)
+        patch :update, params:{ id: nurse.id, nurse: {verified: true}}
+        nurse.reload
+        expect(nurse.verified).to eq false
+      end
+
+      context "when empty fields are submitted" do
+        it "returns the related json errors" do
+          role = Role.create(name: "Test role")
+          nurse = Nurse.create(email: "test@example.com", first_name: "John", last_name: "Doe", phone_number: "074-0000-0000", verified: false, sign_in_count: 0, role_id: role.id)
+          patch :update, params:{ id: nurse.id, nurse: {email:""}}
+          expect(response.body).to eq '{"errors":["Email is invalid"]}'
+        end
+
+        it "returns a 400 response status" do
+          role = Role.create(name: "Test role")
+          nurse = Nurse.create(email: "test@example.com", first_name: "John", last_name: "Doe", phone_number: "074-0000-0000", verified: false, sign_in_count: 0, role_id: role.id)
+          patch :update, params:{ id: nurse.id, nurse: {email:""}}
+          expect(response.status).to eq 400
+        end
+
+        it "does not update the attributes with invalid values" do
+          role = Role.create(name: "Test role")
+          nurse = Nurse.create(email: "test@example.com", first_name: "John", last_name: "Doe", phone_number: "074-0000-0000", verified: false, sign_in_count: 0, role_id: role.id)
+          patch :update, params:{ id: nurse.id, nurse: {first_name: "Bill", email: ""}}
+          expect(nurse.email).to eq "test@example.com"
+        end
       end
     end
 
